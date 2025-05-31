73b675f5cdcce09af0994f648ab0b5259476bfef
Mnigos <makowskiigor@gmail.com>
2025-05-31 14:05:06 +0200

    feat(app/components/navigation-bar): user popover

---
+++ app/components/navigation-bar-user-popover.tsx
@@ -0,0 +1,46 @@
+import type { User } from 'better-auth'
+import { LogOut, User as UserIcon } from 'lucide-react'
+import { authClient } from '~/lib/auth-client'
+import { useChatStore } from '~/stores/chat.store'
+import { Button } from './ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+
+export interface NavigationBarUserPopoverProps {
+	user: Pick<User, 'name' | 'email'>
+}
+
+export function NavigationBarUserPopover({
+	user,
+}: Readonly<NavigationBarUserPopoverProps>) {
+	const clearMessages = useChatStore(state => state.clearMessages)
+
+	async function handleSignOut() {
+		await authClient.signOut()
+		clearMessages()
+		window.location.reload()
+	}
+
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button variant="outline" size="icon" aria-label="User">
+					<UserIcon className="h-4 w-4" />
+				</Button>
+			</PopoverTrigger>
+
+			<PopoverContent className="gap-2">
+				<div>
+					<p className="">{user.name}</p>
+					<p className="max-w-64 truncate text-foreground/50 text-sm">
+						{user.email}
+					</p>
+				</div>
+
+				<Button variant="outline" onClick={handleSignOut}>
+					<LogOut className="h-4 w-4" />
+					Sign Out
+				</Button>
+			</PopoverContent>
+		</Popover>
+	)
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,6 @@
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
 import { Chat } from '~/components/chat'
 import { ShoppingList } from '~/components/shopping-list'
-import { auth } from '~/lib/auth.server'
 import { getQueryClient } from '~/lib/trpc/react'
 import { createTRPC } from '~/lib/trpc/server'
 import type { Route } from './+types/home'
@@ -17,30 +16,7 @@ export async function loader(loaderArgs: Route.LoaderArgs) {
 	const queryClient = getQueryClient()
 	const trpc = await createTRPC(loaderArgs)
 
-	const session = await auth.api.getSession({
-		headers: loaderArgs.request.headers,
-	})
-
-	if (!session) {
-		const { headers: responseHeaders } = await auth.api.signInAnonymous({
-			headers: loaderArgs.request.headers,
-			returnHeaders: true,
-		})
-
-		const setCookieHeader = responseHeaders.get('set-cookie')
-
-		if (setCookieHeader)
-			throw new Response(null, {
-				status: 302,
-				headers: {
-					Location: loaderArgs.request.url,
-					'Set-Cookie': setCookieHeader,
-				},
-			})
-	}
-
-	if (session?.user)
-		await queryClient.prefetchQuery(trpc.shoppingList.getItems.queryOptions())
+	await queryClient.prefetchQuery(trpc.shoppingList.getItems.queryOptions())
 
 	return {
 		queryClient: dehydrate(queryClient),
